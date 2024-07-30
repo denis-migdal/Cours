51479f18cf7d3d2c19b8aebb3bc6ef36b9be0b6f
denmigda <denis.migdal@uca.fr>
2024-07-30 21:11:36 +0200

    Add TPEngine dep

--- src/pages/R1-02/TP/TP1/index.ts
+++ src/pages/R1-02/TP/TP1/index.ts
@@ -16,7 +16,7 @@ for(let pre of document.querySelectorAll('pre[lang="sql"]') ) {
     });
 
     pre.addEventListener("input", (ev) => {
-        
+        // https://stackoverflow.com/questions/21234741/place-caret-back-where-it-was-after-changing-innerhtml-of-a-contenteditable-elem
         let p = ev.target as HTMLElement;
         let rrange = window.getSelection()!.getRangeAt(0);
         let c = rrange.startOffset;
@@ -63,29 +63,4 @@ for(let pre of document.querySelectorAll('pre[lang="sql"]') ) {
         sel.addRange(range);
         //console.log( getCaretCharacterOffsetWithin(ev.target)  );
     });
-}
-
-
-function getCaretCharacterOffsetWithin(element) {
-    var caretOffset = 0;
-    var doc = element.ownerDocument || element.document;
-    var win = doc.defaultView || doc.parentWindow;
-    var sel;
-    if (typeof win.getSelection != "undefined") {
-        sel = win.getSelection();
-        if (sel.rangeCount > 0) {
-            var range = win.getSelection().getRangeAt(0);
-            var preCaretRange = range.cloneRange();
-            preCaretRange.selectNodeContents(element);
-            preCaretRange.setEnd(range.endContainer, range.endOffset);
-            caretOffset = preCaretRange.toString().length;
-        }
-    } else if ( (sel = doc.selection) && sel.type != "Control") {
-        var textRange = sel.createRange();
-        var preCaretTextRange = doc.body.createTextRange();
-        preCaretTextRange.moveToElementText(element);
-        preCaretTextRange.setEndPoint("EndToEnd", textRange);
-        caretOffset = preCaretTextRange.text.length;
-    }
-    return caretOffset;
 }
