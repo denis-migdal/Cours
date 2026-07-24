ccfbdb0ed836e299597e13ee13d6df27faa42c4e
denmigda <denis.migdal@uca.fr>
2026-07-24 15:46:32 +0200

    Update MWL

--- src/pages/templates/page@2026/index.ts
+++ src/pages/templates/page@2026/index.ts
@@ -1,7 +1,7 @@
-import "MWL@2026:Components/page/layout";
-import "MWL@2026:Components/page/lastmodified";
+import "MWL@2026:components/page/layout";
+import "MWL@2026:components/page/lastmodified";
 
-import "MWL@2026:Components/page/menu";
-import "MWL@2026:Components/page/color-switch";
+import "MWL@2026:components/page/menu";
+import "MWL@2026:components/page/color-switch";
 
-import "MWL@2026:Components/code/code-script";
+import "MWL@2026:components/code/code-script";
