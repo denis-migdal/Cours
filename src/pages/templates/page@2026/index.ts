6668faa2e583c3d50bed555d65b315217789d251
denmigda <denis.migdal@uca.fr>
2026-08-26 21:07:49 +0200

    Update libs

--- src/pages/templates/page@2026/index.ts
+++ src/pages/templates/page@2026/index.ts
@@ -1,7 +1,4 @@
-import "MWL@2026/components/page/layout";
-import "MWL@2026/components/page/lastmodified";
+import "MWL@2026/presentation/styles";
+import "MWL@2026/presentation/capabilities";
 
-import "MWL@2026/components/page/menu";
-import "MWL@2026/components/page/color-switch";
-
-import "MWL@2026/components/code/code-script";
+import "MWL@2026/widgets/code/code-script";
